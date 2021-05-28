757eb294e5b8c0ef13ff030aba5323a7143fd6fd
Ezsger <ezsger@gmail.com>
2021-05-28 17:14:54 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [huf, setHuf] = useState('');
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <div className="inputBox">
+        <div className="row">
+          <div className="column-sm">
+            <h2>Deviza váltás</h2>
+          </div>
+        </div>
+        <div className="row">
+          <label name="forint">Forint (HUF)</label>
+
+          <input type="text" name="forint" value={huf} onChange={setHuf} />
+        </div>
+      </div>
     </div>
   );
 }
